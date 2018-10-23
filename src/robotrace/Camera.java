29e421aabfb7ed01e4d492853e788c18c7663ff8
BSanders <b.sanders@student.tue.nl>
2018-10-23 11:54:26 +0200

    Camera die wel werkt

--- src/robotrace/Camera.java
+++ src/robotrace/Camera.java
@@ -27,41 +27,57 @@ class Camera {
             
             // First person mode    
             case 1:
-                setFirstPersonMode(gs, focus, glu);
+                setFirstPersonMode(gs, focus);
                 break;
                 
             // Default mode    
             default:
-                setDefaultMode(gs, glu);
+                setDefaultMode(gs);
         }
     }
 
     /**
      * Computes eye, center, and up, based on the camera's default mode.
      */
-    private void setDefaultMode(GlobalState gs, GLU glu) {
+    private void setDefaultMode(GlobalState gs) {
        /* double xCam = gs.cnt.x - (gs.vDist * sin(gs.theta));
         double yCam = gs.cnt.y - (gs.vDist * cos(gs.phi));
         double zCam = gs.vDist;*/
-        //double xCam = gs.cnt.x + gs.vDist * cos(gs.phi);
         
-        double xCam = gs.cnt.x + gs.vDist * cos(gs.theta) * sin(gs.phi);
-        double yCam = gs.cnt.y + gs.vDist * sin(gs.theta) * sin(gs.phi);
-        double zCam = gs.cnt.z + gs.vDist * cos(gs.phi);
+       //Spherical coordinates of the camera
+        double xCam = gs.cnt.x + gs.vDist * cos(gs.theta) * cos(gs.phi);
+        double yCam = gs.cnt.y + gs.vDist * sin(gs.theta) * cos(gs.phi);
+        double zCam = gs.cnt.z + gs.vDist * sin(gs.phi);
+        eye = new Vector(xCam, yCam, zCam);
+        //Coordinates where the camera looks at
         double xCentralPoint = gs.cnt.x;
         double yCentralPoint = gs.cnt.y;
         double zCentralPoint = gs.cnt.z;
-        
-        glu.gluLookAt(xCam, yCam, zCam, 
-                xCentralPoint, yCentralPoint, zCentralPoint, 
-                0.0, 0.0, 1.0);
+        center = new Vector(xCentralPoint, yCentralPoint, zCentralPoint);
+        up = new Vector(0.0, 0.0, 1.0);
+//        glu.gluLookAt(xCam, yCam, zCam, 
+//                xCentralPoint, yCentralPoint, zCentralPoint, 
+//                0.0, 0.0, 1.0);
     }
 
     /**
      * Computes eye, center, and up, based on the first person mode.
      * The camera should view from the perspective of the robot.
      */
-    private void setFirstPersonMode(GlobalState gs, Robot focus, GLU glu) {
+    private void setFirstPersonMode(GlobalState gs, Robot focus) {
+        //focus.position is a vector which gives the position of the robot
+        double posRobotx = focus.position.x;
+        double posRoboty = focus.position.y;
+        double posRobotz = focus.position.z;
         
+        //focus.direction is a vector which gives the direction the robot faces
+        double lookRobotx = focus.direction.x;
+        double lookRoboty = focus.direction.y;
+        double lookRobotz = focus.direction.z;
+        
+        
+        eye = new Vector(posRobotx, posRoboty, posRobotz);
+        center = new Vector(lookRobotx, lookRoboty, lookRobotz);
+        up= new Vector(0.0, 0.0, 1.0);
     }
 }
