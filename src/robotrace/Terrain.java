c2ad80a1df0adf331ee21248484c2141bce957c3
BSanders <b.sanders@student.tue.nl>
2018-10-23 22:11:42 +0200

    Update src/robotrace/Terrain.java

--- src/robotrace/Terrain.java
+++ src/robotrace/Terrain.java
@@ -12,53 +12,52 @@ class Terrain {
     
     
     public Terrain() {
-        double height = 0.6 * cos(0.3 * x + 0.2 * y) + 0.4 * cos(x - 0.5 * y);
     }
 
     /**
      * Draws the terrain.
      */
-    public void draw(GL2 gl, GLU glu, GLUT glut, double height) {
-        drawFieldOne(gl, glu, glut, height);
-        drawFieldTwo(gl, glu, glut, height);
-        drawFieldThree(gl, glu, glut, height);
-        drawFieldFour(gl, glu, glut, height);
-        drawFieldFive(gl, glu, glut, height);
+    public void draw(GL2 gl, GLU glu, GLUT glut) {
+        drawFieldOne(gl, glu, glut);
+        drawFieldTwo(gl, glu, glut);
+        drawFieldThree(gl, glu, glut);
+        drawFieldFour(gl, glu, glut);
+        drawFieldFive(gl, glu, glut);
         
     }
     
-    public void drawFieldOne(GL2 gl, GLU glu, GLUT glut, double height) {
+    public void drawFieldOne(GL2 gl, GLU glu, GLUT glut) {
         gl.glPushMatrix();
         gl.glTranslated(10, -10, 0);
-        gl.glScaled(20, 20, height);
+        gl.glScaled(20, 20, 0);
         gl.glPopMatrix();
     }
     
-    public void drawFieldTwo(GL2 gl, GLU glu, GLUT glut, double height) {
+    public void drawFieldTwo(GL2 gl, GLU glu, GLUT glut) {
         gl.glPushMatrix();
         gl.glTranslated(-10, -10, 0);
-        gl.glScaled(20, 20, height);
+        gl.glScaled(20, 20, 0);
         gl.glPopMatrix();
     }
     
-    public void drawFieldThree(GL2 gl, GLU glu, GLUT glut, double height) {
+    public void drawFieldThree(GL2 gl, GLU glu, GLUT glut) {
         gl.glPushMatrix();
         gl.glTranslated(-10, 10, 0);
-        gl.glScaled(20, 20, height);
+        gl.glScaled(20, 20, 0);
         gl.glPopMatrix();
     }
     
-    public void drawFieldFour(GL2 gl, GLU glu, GLUT glut, double height) {
+    public void drawFieldFour(GL2 gl, GLU glu, GLUT glut) {
         gl.glPushMatrix();
         gl.glTranslated(5, 10, 0);
-        gl.glScaled(10, 20, height);
+        gl.glScaled(10, 20, 0);
         gl.glPopMatrix();
     }
     
-    public void drawFieldFive(GL2 gl, GLU glu, GLUT glut, double height) {
+    public void drawFieldFive(GL2 gl, GLU glu, GLUT glut) {
         gl.glPushMatrix();
         gl.glTranslated(15, 10, 0);
-        gl.glScaled(10, 20, height);
+        gl.glScaled(10, 20, 0);
         gl.glPopMatrix();
     }
 }
